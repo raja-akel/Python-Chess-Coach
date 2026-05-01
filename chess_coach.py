d1c14d3e8557ec2dae3efc1082d37fd03931eb0f
raja-akel <rajakel99@gmail.com>
2026-05-01 02:11:56 +0200

    Working input and output systems (basic move inputs in SAN format, basic output into a board, and basic output of moves in a readable way)

---
+++ chess_coach.py
@@ -0,0 +1,10 @@
+import chess
+from stockfish import Stockfish
+
+stockfish = Stockfish()
+stockfish.set_skill_level(10)
+stockfish.set_depth(15)
+stockfish.get_top_moves(3)
+
+chess_database_pgn = open("C:\\Users\\user\\PycharmProjects\\Databases\\lichess_db_standard_rated_2017-01.pgn\\lichess_db_standard_rated_2017-01.pgn")
+
--- testing_ground.py
+++ testing_ground.py
@@ -1,7 +1,12 @@
 import chess
 import chess.pgn
 import random
+from stockfish import Stockfish
 
+stockfish = Stockfish("C:\\Users\\user\\PycharmProjects\\Databases\\stockfish-windows-x86-64-avx2\\stockfish\\stockfish-windows-x86-64-avx2.exe")
+stockfish.set_skill_level(10)
+stockfish.set_depth(15)
+stockfish.get_top_moves(3)
 
 
 from my_functions import *
@@ -11,37 +16,111 @@ board.reset()
 
 x = 0
 
-while not (x == 1) :
-    print("\n\n\n\n\n\n")
+game_mode = input("Choose game mode(1p or 2p): ")
+
+if game_mode == "2p":
+    while not (x == 1) :
+        print("\n\n\n\n\n\n")
+
+        print_board(board)
+        print_legal_moves(board)
+
+        while True:
+            w_move = input("White move: ")
+            try:
+                temp_move = board.parse_san(w_move)
+                if board.is_legal(temp_move):
+                    board.push(temp_move)
+                    break
+                else:
+                    print("Illegal move")
+            except:
+                print("Illegal move")
+
+
+
+        if board.is_game_over():
+            x = 1
+            print("\n\n\n\n\n\n")
+            #print(board.outcome())
+            print_board(board)
+            break
+
+        print("\n\n\n\n\n\n")
+
+        print_board(board)
+        print_legal_moves(board)
+
+        while True:
+            b_move = input("Black move: ")
+            try:
+                temp_move = board.parse_san(b_move)
+                if board.is_legal(temp_move):
+                    board.push(temp_move)
+                    break
+                else:
+                    print("Illegal move")
+            except:
+                print("Illegal move")
+
+        if board.is_game_over():
+            x = 1
+            print("\n\n\n\n\n\n")
+            #print(board.outcome())
+            print_board(board)
+            break
 
     print_board(board)
-    print_legal_moves(board)
+    print(board.status())
 
-    w_move = input("White move: ")
-    temp_move = board.parse_san(w_move)
 
-    if board.is_legal(temp_move):
-        board.push(temp_move)
-    else:
-        print("Illegal move")
-        break
 
-    print("\n\n\n\n\n\n")
+
+elif game_mode == "1p":
+    while not (x == 1):
+        print("\n\n\n\n\n\n")
+
+        print_board(board)
+        print_legal_moves(board)
+
+        while True:
+            w_move = input("White move: ")
+            try:
+                temp_move = board.parse_san(w_move)
+                if board.is_legal(temp_move):
+                    board.push(temp_move)
+                    break
+                else:
+                    print("Illegal move")
+            except:
+                print("Illegal move")
+        if board.is_game_over():
+            x = 1
+            print("\n\n\n\n\n\n")
+            # print(board.outcome())
+            print_board(board)
+            break
+
+        print("\n\n\n\n\n\n")
+
+        print_board(board)
+
+        temp_fen_position = board.fen()
+        stockfish.set_fen_position(temp_fen_position, True)
+        temp_move = board.parse_uci(stockfish.get_best_move())
+
+        if board.is_legal(temp_move):
+            board.push(temp_move)
+        else:
+            print("Illegal move")
+            break
+
+        if board.is_game_over():
+            x = 1
+            print("\n\n\n\n\n\n")
+            # print(board.outcome())
+            print_board(board)
+            break
 
     print_board(board)
-    print_legal_moves(board)
-
-    b_move = input("Black move: ")
-    temp_move = board.parse_san(b_move)
-
-    if board.is_legal(temp_move):
-        board.push(temp_move)
-    else:
-        print("Illegal move")
-        break
-
-
-
-
-print_board(board)
-print(board.status())
+    print(board.status())
